e9312608765ea6bea0dce44ed3da7bc2b0976be5
Martin Jose Garcia Muñoz <martinillora@gmail.com>
2024-09-23 18:47:38 +0200

    Added Favorite View and Navigation
    Added Favorite View and Navigation

---
+++ src/components/FavoritePokemon/FavoritePokemon.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { useFavoriteStore } from "../../store/useFavoriteStore"
+import { PokemonCard } from "../PokemonCard/PokemonCard";
+import { Grid } from "../shared/Grid/Grid";
+
+export const FavoritePokemon = () => {
+    const favoriteIds = useFavoriteStore((state) => state.favorites);
+
+    return (
+        <Grid>
+            {favoriteIds.map((favoriteId) => <PokemonCard key={favoriteId} pokemonId={Number(favoriteId)}/>)}
+        </Grid>
+    )
+}
