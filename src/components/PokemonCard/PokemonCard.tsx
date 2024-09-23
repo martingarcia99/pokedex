0a4a0a4da571ff7ce998526b91b72eb35c1680e3
Martin Jose Garcia Muñoz <martinillora@gmail.com>
2024-09-23 14:39:23 +0200

    Initial features
    Added a card and list componentes and several config and functionalities

---
+++ src/components/PokemonCard/PokemonCard.tsx
@@ -0,0 +1,29 @@
+import React, { useMemo } from 'react';
+import useGetPokemon from '../../hooks/useGetPokemon';
+import { PokemonListItem } from '../../interfaces/PokemonListItem';
+import { getMainPokemonType } from '../../utils/getMainPokemonType';
+import { Label } from '../shared/Label/Label';
+import { capitilizeFirstLetter } from '../../utils/capitilizeFirstLetter';
+
+interface PokemonCardProps {
+    pokemon?: PokemonListItem;
+    pokemonId?: number;
+}
+
+export const PokemonCard: React.FC<PokemonCardProps> = ({ pokemon, pokemonId }) => {
+    const { pokemonData } = useGetPokemon(pokemon?.name, pokemonId);
+    const mainType = useMemo(() => pokemonData && getMainPokemonType(pokemonData), [pokemonData]);
+    
+    return (
+        <div className={`${mainType}-background w-56 h-56 rounded-lg shadow-lg p-4`}>
+            <div className='flex flex-col items-center mx-auto'>
+                <Label>{pokemonData?.name ? capitilizeFirstLetter(pokemonData?.name) : ''}</Label>
+                <img 
+                    src={pokemonData?.sprites?.front_default} 
+                    alt={pokemonData?.name?? ''} 
+                    className='mx-auto w-40 h-40'
+                />
+            </div>
+        </div>
+    );
+}
